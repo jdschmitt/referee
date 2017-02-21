ffa1fa7b8a1235ec37230231b78e8eafdd893d4c
Jason Schmitt <jason.schmitt@gmail.com>
2017-02-20 22:23:54 -0600

    Correcting mismatched column data types

--- src/main/resources/db/migration/V3_2__Connect_game_to_season.sql
+++ src/main/resources/db/migration/V3_2__Connect_game_to_season.sql
@@ -1,14 +1,27 @@
 -- Alter games to point to season
 
 ALTER TABLE `game`
-  ADD COLUMN `season_id` BIGINT(20) NOT NULL AFTER `game_type`,
-  ADD INDEX `game_season_id_fk` (`season_id` ASC);
+  ADD COLUMN `season_id` INT(10) UNSIGNED NOT NULL AFTER `game_type`,
+  ADD INDEX `game_season_id_fk_idx` (`season_id` ASC);
 
 UPDATE `game` SET `season_id` = 1;
 
 ALTER TABLE `game`
-  ADD CONSTRAINT `game_season_fk`
+  ADD CONSTRAINT `game_season_id_fk`
 FOREIGN KEY (`season_id`)
-REFERENCES `player` (`id`)
+REFERENCES `season` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ALTER TABLE `settings`
+  ADD COLUMN `current_season_id` INT(10) UNSIGNED NOT NULL AFTER `winners_per_week`,
+  ADD INDEX `settings_current_season_id_fk_idx` (`current_season_id` ASC);
+
+UPDATE `settings` SET `current_season_id` = 1;
+
+ALTER TABLE `settings`
+  ADD CONSTRAINT `settings_current_season_id_fk`
+FOREIGN KEY (`current_season_id`)
+REFERENCES `season` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
--- src/main/resources/db/migration/V3__Create_season_table.sql
+++ src/main/resources/db/migration/V3__Create_season_table.sql
@@ -1,7 +1,7 @@
 -- Create new table
 
 CREATE TABLE `season` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_reg_game_date` datetime NOT NULL,
   `first_playoff_game_date` datetime NOT NULL,
   `super_bowl_date` datetime NOT NULL,
