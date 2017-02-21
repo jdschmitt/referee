13b66a88c1a3ae675bdd40e4e010323d9cc96852
Jason Schmitt <jason.schmitt@gmail.com>
2017-02-20 22:30:00 -0600

    Explicit id on insert

--- src/main/resources/db/migration/V3_1__Populate_season.sql
+++ src/main/resources/db/migration/V3_1__Populate_season.sql
@@ -1,5 +1,5 @@
 
 -- Add a season record
 
-INSERT INTO `season` (first_reg_game_date, first_playoff_game_date, super_bowl_date)
-VALUES ('2016-09-08 00:00:00','2016-01-05 00:00:00','2016-02-02 00:00:00');
+INSERT INTO `season` (id, first_reg_game_date, first_playoff_game_date, super_bowl_date)
+VALUES (1, '2016-09-08 00:00:00','2016-01-05 00:00:00','2016-02-02 00:00:00');
