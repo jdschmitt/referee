57659a78a8f9baf0b38afb6233e041252703ad87
Jason Schmitt <jason.schmitt@gmail.com>
2017-02-20 22:03:39 -0600

    Cleanup migrations

--- src/main/resources/db/migration/V3_2__Connect_game_to_season.sql
+++ src/main/resources/db/migration/V3_2__Connect_game_to_season.sql
@@ -1,11 +1,14 @@
 -- Alter games to point to season
 
-ALTER TABLE `pickem`.`game`
-  ADD COLUMN `season_id` BIGINT(20) NOT NULL DEFAULT 1 AFTER `game_type`,
+ALTER TABLE `game`
+  ADD COLUMN `season_id` BIGINT(20) NOT NULL AFTER `game_type`,
   ADD INDEX `game_season_id_fk` (`season_id` ASC);
-ALTER TABLE `pickem`.`game`
+
+UPDATE `game` SET `season_id` = 1;
+
+ALTER TABLE `game`
   ADD CONSTRAINT `game_season_fk`
 FOREIGN KEY (`season_id`)
-REFERENCES `pickem`.`player` (`id`)
+REFERENCES `player` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
