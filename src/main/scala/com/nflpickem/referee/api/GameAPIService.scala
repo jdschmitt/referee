c6b66168fb741367e799da70f863f7e7c3f01f17
Jason Schmitt <jason.schmitt@gmail.com>
2017-05-13 19:08:41 -0500

    Added helpful endpoints

--- src/main/scala/com/nflpickem/referee/api/GameAPIService.scala
+++ src/main/scala/com/nflpickem/referee/api/GameAPIService.scala
@@ -1,20 +1,22 @@
 package com.nflpickem.referee.api
 
+import com.nflpickem.referee.Whistle
 import com.nflpickem.referee.model.Game
-import com.nflpickem.referee.service.GameService
+import com.nflpickem.referee.service.{GameService, SeasonService}
+import spray.http.StatusCodes
 import spray.routing.{HttpService, Route}
 
 /**
   * Created by jason on 3/3/17.
   */
-trait GameAPIService extends HttpService {
+trait GameAPIService extends HttpService with Whistle {
 
   import com.nflpickem.referee.model.ApiFormats._
   import spray.httpx.SprayJsonSupport._
 
   def gamesRoute: Route =
     pathPrefix("api") {
-      path("games") {
+      pathPrefix("games") {
         pathEndOrSingleSlash {
           get {
             parameters('week) { week =>
@@ -28,6 +30,29 @@ trait GameAPIService extends HttpService {
               complete {
                 GameService.insertGame(game)
               }
+            }
+          }
+        } ~
+        path(LongNumber) { id =>
+          delete {
+            complete {
+              if (GameService.deleteGame(id))
+                StatusCodes.NoContent
+              else {
+                log.error(s"Failed to delete game $id")
+                StatusCodes.InternalServerError
+              }
+            }
+          } ~
+          get {
+            complete(GameService.getGame(id))
+          }
+        } ~
+        path("season" / LongNumber) { id =>
+          get {
+            complete {
+              val seasonId: Long = SeasonService.currentSeason.get.id.get
+              GameService.getGamesForSeason(id)
             }
           }
         }
--- src/main/scala/com/nflpickem/referee/service/GameService.scala
+++ src/main/scala/com/nflpickem/referee/service/GameService.scala
@@ -51,4 +51,19 @@ object GameService extends Whistle {
     selectStmt.map(Game.fromDb).list().apply()
   }
 
+  def deleteGame(id: Long): Boolean = DB.autoCommit { implicit session =>
+    sql"DELETE FROM game WHERE id = $id;"
+      .update().apply() == 1
+  }
+
+  def getGame(id: Long): Option[Game] = DB.readOnly { implicit session =>
+    sql"SELECT * FROM game WHERE id = $id"
+        .map(Game.fromDb).single().apply()
+  }
+
+  def getGamesForSeason(seasonId: Long): Seq[Game] = DB.readOnly { implicit session =>
+    sql"SELECT * FROM game WHERE season_id = $seasonId ORDER BY game_time ASC;"
+        .map(Game.fromDb).list().apply()
+  }
+
 }
