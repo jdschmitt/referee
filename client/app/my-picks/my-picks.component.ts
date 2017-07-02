c0a346f4e7eeee34c1eee3b0e11227983e294af6
Jason Schmitt <jason.schmitt@gmail.com>
2017-07-03 02:36:34 +1000

    Added / updated week number header to some components

--- client/app/my-picks/my-picks.component.ts
+++ client/app/my-picks/my-picks.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {BaseComponent} from '../base.component';
+import {SettingsService} from "../services/settings.service";
 
 @Component({
   selector: 'app-my-picks',
   templateUrl: './my-picks.component.html',
   styleUrls: ['./my-picks.component.css']
 })
-export class MyPicksComponent implements OnInit {
+export class MyPicksComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected settingsService: SettingsService
+  ) {
+    super(settingsService);
+  }
 
   ngOnInit() {
   }
