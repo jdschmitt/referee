98a0e0feda80729002a0deddfb59f97b234167fa
Jason Schmitt <jason.schmitt@gmail.com>
2017-07-13 22:47:49 -0500

    Fix initial, default season record dates

--- src/main/resources/db/migration/V2_1__Populate_season.sql
+++ src/main/resources/db/migration/V2_1__Populate_season.sql
@@ -2,4 +2,4 @@
 -- Add a season record
 
 INSERT INTO `season` (id, first_reg_game_date, first_playoff_game_date, super_bowl_date)
-VALUES (1, '2016-09-08 00:00:00','2016-01-05 00:00:00','2016-02-02 00:00:00');
+VALUES (1, '2016-09-08 00:00:00','2017-01-05 00:00:00','2017-02-02 00:00:00');
