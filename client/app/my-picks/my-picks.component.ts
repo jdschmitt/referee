affd089017e7fd1fde3c92f0ba61fb82847b8b7d
Jason Schmitt <jason.schmitt@gmail.com>
2017-07-16 19:51:30 -0500

    Displaying games on My Picks page

--- client/app/my-picks/my-picks.component.ts
+++ client/app/my-picks/my-picks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {BaseComponent} from '../base.component';
-import {SettingsService} from "../services/settings.service";
+import { BaseComponent } from '../base.component';
+import { GamesService } from '../services/games.service';
+import { SettingsService } from "../services/settings.service";
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-my-picks',
@@ -10,12 +12,37 @@ import {SettingsService} from "../services/settings.service";
 export class MyPicksComponent extends BaseComponent implements OnInit {
 
   constructor(
-    protected settingsService: SettingsService
+    protected settingsService: SettingsService,
+    private gamesService: GamesService
   ) {
     super(settingsService);
   }
 
+  games: any[];
+
   ngOnInit() {
+    this.loadGames();
+  }
+
+  // TODO probably don't really need this but need to adjust when user changes which week they want to view
+  onCurrentWeekChange() {
+    this.loadGames();
+  }
+
+  loadGames() {
+    if (_.isUndefined(this.currentWeek))
+      return;
+
+    this.gamesService.getGamesForWeek(this.currentWeek).subscribe(
+      data => {
+        this.games = data;
+      },
+      error => console.log("Error calling /games: ", error)
+    );
+  }
+
+  submitPicks() {
+    console.log("submitting picks");
   }
 
 }
